a210a45f160df6c60d0c14410daba72365103160
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-20 19:56:02 +0100

    R!! Strip out all the implementation, ready for demoing

--- 02Reporters/svg_test.cpp
+++ 02Reporters/svg_test.cpp
@@ -1,27 +1,14 @@
 #include "SVGDrawing.h"
-#include "SVGReporter.h"
 
 #include <catch2/catch.hpp>
 #include <ApprovalTests.hpp>
 
 using namespace ApprovalTests;
 
-void verifyDrawing(const SVGDrawing& drawing)
-{
-    Approvals::verify(drawing.content(),
-                      Options(SVGReporter())
-                          .fileOptions()
-                          .withFileExtension(".svg"));
-}
-
 TEST_CASE("SVG Test")
 {
     SVGDrawing drawing("drawing1.svg");
-    verifyDrawing(drawing);
+    // TODO 1 Test the complete content of this file
 }
 
-TEST_CASE("SVG Test 2")
-{
-    SVGDrawing drawing("drawing2.svg");
-    verifyDrawing(drawing);
-}
+// TODO 2 Also test "drawing2.svg"
