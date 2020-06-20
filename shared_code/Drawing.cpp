2fd907459e6d3d54102ee120f4470027a1475176
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-20 19:24:53 +0100

    F   Add disabled hack to simulate a future mainteance change

--- shared_code/Drawing.cpp
+++ shared_code/Drawing.cpp
@@ -1,5 +1,6 @@
 #include "Drawing.h"
 #include "FileUtilities.h"
+#include "StringUtilities.h"
 
 #include <filesystem>
 
@@ -13,5 +14,15 @@ std::string Drawing::content() const
     std::filesystem::path sourceFile(__FILE__);
     auto directory = sourceFile.remove_filename();
     auto svgFile = directory / "resources" / m_source_svg_file;
-    return FileUtilities::readFileThrowIfMissing(svgFile);
+
+    std::string content =
+        FileUtilities::readFileThrowIfMissing(svgFile);
+
+    if (false)
+    {
+        // Hack to break the output, to demo test failure
+        content = StringUtilities::replaceAll(
+            content, "3.202323", "3.302323");
+    }
+    return content;
 }
