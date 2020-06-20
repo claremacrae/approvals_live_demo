4e3bf94b07f1b6945b526615fd1513ebd5d55f29
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-20 19:51:03 +0100

    t   Extract helper function

--- 02Reporters/svg_test.cpp
+++ 02Reporters/svg_test.cpp
@@ -5,13 +5,18 @@
 
 using namespace ApprovalTests;
 
-TEST_CASE("SVG Test")
+void verifyDrawing(const SVGDrawing& drawing)
 {
-    SVGDrawing drawing("drawing1.svg");
     Approvals::verify(drawing.content(),
                       Options(Mac::BeyondCompareReporter())
                           .fileOptions()
                           .withFileExtension(".svg"));
+}
+
+TEST_CASE("SVG Test")
+{
+    SVGDrawing drawing("drawing1.svg");
+    verifyDrawing(drawing);
 }
 
 // TODO Test "drawing1.svg" as well
