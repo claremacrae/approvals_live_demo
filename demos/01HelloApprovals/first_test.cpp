ff472eb78f8a6de8034a30c72f27e4bb57918bbb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-22 16:32:18 +0100

    c   Clarify TODOs

--- demos/01HelloApprovals/first_test.cpp
+++ demos/01HelloApprovals/first_test.cpp
@@ -1,7 +1,7 @@
 #include "HelloApprovals.h"
 #include <catch2/catch.hpp>
 
-// TODO 1 Fix this test
+// TODO 1 Run and then fix this test
 // TODO 2 Convert this to an approval test
 
 TEST_CASE( "First Test" )
