7ce0801252e65cb0663911653446446d018f2be2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-22 09:30:42 +0100

    t   Make the test fail initially

--- demos/HelloApprovals/first_test.cpp
+++ demos/HelloApprovals/first_test.cpp
@@ -1,9 +1,10 @@
 #include "HelloApprovals.h"
 #include <catch2/catch.hpp>
 
-// TODO Convert this to an approval test
+// TODO 1 Fix this test
+// TODO 2 Convert this to an approval test
 
 TEST_CASE("First Test")
 {
-    CHECK(HelloApprovals::helloApprovals() == "Hello Approvals");
+    CHECK(HelloApprovals::helloApprovals() == "?");
 }
