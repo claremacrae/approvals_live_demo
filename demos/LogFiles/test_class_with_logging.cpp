a3408b0e55e6b42bfc4be4d3508161dcdf5aafe9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-22 08:28:01 +0100

    t   Revert test_class_with_logging.cpp back to initial state

--- demos/LogFiles/test_class_with_logging.cpp
+++ demos/LogFiles/test_class_with_logging.cpp
@@ -1,37 +1,13 @@
 #include "HelloApprovals.h"
 #include <catch2/catch.hpp>
-#include "ApprovalTests.hpp"
 
 #include "ComplexOperation.h"
-
-using namespace ApprovalTests;
+#include <iostream>
 
 // TODO Write a test for this
 
 TEST_CASE("Test ComplexOperation Log")
 {
     ComplexOperation stuff;
-
-    // Get a filename
-    const auto fileName =
-        Approvals::getDefaultNamer()->getReceivedFile(".log");
-
-    {
-        // Create a stream to send the output to
-        std::ofstream stream(fileName);
-
-        // Do stuff
-        stuff.doStuff(stream);
-
-        // the log file is closed when the stream goes out of scope
-    }
-
-    // Verify the contents of the log from doing stuff
-    const auto dateRegex =
-        R"([A-Za-z]{3} [A-Za-z]{3} [\d ]\d \d\d:\d\d:\d\d \d\d\d\d)";
-
-    Approvals::verifyExistingFile(
-        fileName,
-        Options(Scrubbers::createRegexScrubber(dateRegex,
-                                               "[date_and_time]")));
+    stuff.doStuff(std::cout);
 }
