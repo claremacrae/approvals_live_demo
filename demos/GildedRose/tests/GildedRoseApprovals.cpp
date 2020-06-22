0cac5ef716f9c9e3461476536e31012201c8a4c1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-22 12:27:10 +0100

    c   Remove some comments that are in the README.md

--- demos/GildedRose/tests/GildedRoseApprovals.cpp
+++ demos/GildedRose/tests/GildedRoseApprovals.cpp
@@ -9,10 +9,6 @@ using namespace ApprovalTests;
 /* TODO: Use ApprovalTests to get to 100% line and branch coverage
          of GildedRose.cc.
 
-   For help setting up code-coverage measurements, see:
-    * https://github.com/claremacrae/talks/blob/main/HowTos/Set_up_Code_Coverage_on_Windows.md
-    * https://github.com/claremacrae/talks/blob/main/HowTos/Set_up_Code_Coverage_on_macOS.md
-
    NOTE: All tests are disabled initially, with Catch's "[!hide]" tag
 */
 
