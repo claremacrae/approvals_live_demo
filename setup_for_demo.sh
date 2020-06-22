3e578744d8d38c8db20846b0774f8c7a570604a6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-22 13:55:07 +0100

    e   Delete more results from previous runs

--- setup_for_demo.sh
+++ setup_for_demo.sh
@@ -4,3 +4,5 @@ delete_coverage_gcda_files.sh
 
 rm -f demos/HelloApprovals/first_test.First_Test.*.txt
 rm -f demos/GildedRose/tests/approval_tests/*.txt
+rm -f demos/LogFiles/approval_tests/*
+rm -f demos/SVGFiles/approval_tests/*
