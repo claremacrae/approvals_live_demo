edbeab483c6f07f15495a60c04a08760502f4d3a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-23 06:49:14 +0100

    e   Don't delete a file that is now version-controlled

--- setup_for_demo.sh
+++ setup_for_demo.sh
@@ -5,4 +5,4 @@ delete_coverage_gcda_files.sh
 rm -f demos/01HelloApprovals/first_test.First_Test.*.txt
 rm -f demos/02GildedRose/tests/approval_tests/*.txt
 rm -f demos/03LogFiles/approval_tests/*
-rm -f demos/04SVGFiles/approval_tests/*
+#rm -f demos/04SVGFiles/approval_tests/*
