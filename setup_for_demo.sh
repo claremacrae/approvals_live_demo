90bb198004ea01bdffa75fd60936927a2c6adcbf
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-23 07:36:14 +0100

    e   Update files names in setup_for_demo.sh

--- setup_for_demo.sh
+++ setup_for_demo.sh
@@ -2,7 +2,7 @@
 
 delete_coverage_gcda_files.sh
 
-rm -f demos/01HelloApprovals/first_test.First_Test.*.txt
+rm -f demos/01HelloApprovals/HelloApprovalsTests.helloPoetry.*.txt
 rm -f demos/02GildedRose/tests/approval_tests/*.txt
 rm -f demos/03LogFiles/approval_tests/*
-#rm -f demos/04SVGFiles/approval_tests/*
+rm -f demos/04SVGFiles/approval_tests/*.received.*
