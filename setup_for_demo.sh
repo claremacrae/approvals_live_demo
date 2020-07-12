608c4ac1bf8c180422bd10b17b677292300c7b59
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-12 15:25:30 +0100

    e   Clean out results from demo 5

--- setup_for_demo.sh
+++ setup_for_demo.sh
@@ -6,4 +6,4 @@ rm -f demos/01HelloApprovals/HelloApprovalsTests.helloPoetry.*.txt
 rm -f demos/02GildedRose/tests/approval_tests/*.txt
 rm -f demos/03LogFiles/approval_tests/*
 rm -f demos/04SVGFiles/approval_tests/*.received.*
-rm -f demos/05SVGFiles/approval_tests/
+rm -rf demos/05QtWidgets/approval_tests/
