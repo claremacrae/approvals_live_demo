4da86ff96b6d4a9744ded62a37b13809d89185ed
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-12 16:05:16 +0100

    t   Easier to expose Demo5 in sections

--- demos/05QtWidgets/WidgetsTest.cpp
+++ demos/05QtWidgets/WidgetsTest.cpp
@@ -36,14 +36,18 @@ TEST_CASE( "It approves QImages" )
         QImage image = QImageExamples::createImage( color );
         ApprovalTestsQt::verifyQImage( image );
     }
+}
+ */
 
+/*
     // TODO 4: Write each image to a separate file
     // Catch2: SECTION()
     //  https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/MultipleOutputFilesPerTest.html
+*/
 
+/*
     // TODO 5: Make it easier to approve the output
     // a) AutoApproveReporter
     // b) SeparateApprovedAndReceivedDirectoriesNamer
     //  https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/Namers.html#separateapprovedandreceiveddirectoriesnamer
-}
- */
+*/
