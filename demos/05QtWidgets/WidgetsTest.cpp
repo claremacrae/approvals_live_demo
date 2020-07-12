a213cb65847b1d13db87f4ef88a0fc2f0db553a1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-12 14:30:29 +0100

    t   Add some comments

--- demos/05QtWidgets/WidgetsTest.cpp
+++ demos/05QtWidgets/WidgetsTest.cpp
@@ -7,12 +7,12 @@
 #include "QImageExamples.h"
 #include "QTableWidgetExamples.h"
 
+// Scenario: Some code that mixes doing some calculations
+//           with putting the results in to a table widget.
+// TODO 1: Use ApprovalTestsQt to save the table content
+// TODO 2: Review verifyQTableView()
 TEST_CASE( "It approves a QTableWidget" )
 {
-    // A note on naming: QTableWidget is a concrete class that implements
-    // the more general QTableView. Here we create a QTableWidget,
-    // for convenience.
-
     QTableWidget tableWidget;
     QTableWidgetExamples::populateTable( tableWidget );
     ApprovalTestsQt::verifyQTableView( tableWidget );
