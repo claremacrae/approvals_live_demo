5198dc41269f91935b57219b95a7626720a1f749
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-05 12:59:57 +0100

    Complete example 2

--- demos/02GildedRose/tests/GildedRoseApprovals.cpp
+++ demos/02GildedRose/tests/GildedRoseApprovals.cpp
@@ -19,23 +19,23 @@ using namespace ApprovalTests;
            Approvals::verifyAll( std::vector<Item> );
 */
 
-//TEST_CASE( "VerifyCombinations" )
-//{
-//    std::vector<std::string> names{
-//        "Wibble",
-//        "Aged Brie",
-//        "Backstage passes to a TAFKAL80ETC concert" }; // Added
-//    std::vector<int> sellIns{ 0 };
-//    std::vector<int> qualities{ 0, 1, 2 }; // Added: 1, 2
-//
-//    CombinationApprovals::verifyAllCombinations(
-//        []( const std::string& name, int sellIn, int quality ) {
-//            return getUpdatedItem( name, sellIn, quality );
-//        },
-//        names,
-//        sellIns,
-//        qualities ); // 100% line coverage
-//}
+TEST_CASE( "VerifyCombinations" )
+{
+    std::vector<std::string> names{
+        "Wibble",
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert" }; // Added
+    std::vector<int> sellIns{ 0 };
+    std::vector<int> qualities{ 0, 1, 2 }; // Added: 1, 2
+
+    CombinationApprovals::verifyAllCombinations(
+        []( const std::string& name, int sellIn, int quality ) {
+            return getUpdatedItem( name, sellIn, quality );
+        },
+        names,
+        sellIns,
+        qualities ); // 100% line coverage
+}
 
 /* Approved file starts with:
 
@@ -51,21 +51,21 @@ using namespace ApprovalTests;
 // TODO 4: Use ApprovalTests to get from 75% to 100% BRANCH coverage
 //         in GildedRose.cc.
 
-//TEST_CASE( "VerifyMoreCombinations" )
-//{
-//    std::vector<std::string> names{
-//        "Wibble",
-//        "Aged Brie",
-//        "Backstage passes to a TAFKAL80ETC concert",
-//        "Sulfuras, Hand of Ragnaros" };            // Added
-//    std::vector<int> sellIns{ -1, 0, 11 };         // Added -1, 11
-//    std::vector<int> qualities{ 0, 1, 2, 49, 50 }; // Added 49. 50
-//
-//    CombinationApprovals::verifyAllCombinations(
-//        []( const std::string& name, int sellIn, int quality ) {
-//            return getUpdatedItem( name, sellIn, quality );
-//        },
-//        names,
-//        sellIns,
-//        qualities ); // 100% line coverage, 100% branch coverage
-//}
+TEST_CASE( "VerifyMoreCombinations" )
+{
+    std::vector<std::string> names{
+        "Wibble",
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert",
+        "Sulfuras, Hand of Ragnaros" };            // Added
+    std::vector<int> sellIns{ -1, 0, 11 };         // Added -1, 11
+    std::vector<int> qualities{ 0, 1, 2, 49, 50 }; // Added 49. 50
+
+    CombinationApprovals::verifyAllCombinations(
+        []( const std::string& name, int sellIn, int quality ) {
+            return getUpdatedItem( name, sellIn, quality );
+        },
+        names,
+        sellIns,
+        qualities ); // 100% line coverage, 100% branch coverage
+}
