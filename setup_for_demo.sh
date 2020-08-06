7e590c611a87d9b0b4408e8ecf7577078c0ef369
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-06 15:46:31 +0100

    Delete unneeded approval_tests folders

--- setup_for_demo.sh
+++ setup_for_demo.sh
@@ -4,7 +4,7 @@ delete_coverage_gcda_files.sh
 
 rm -f  demos/01HelloApprovals/HelloApprovalsTests.helloPoetry.*.txt
 rm -rf demos/02GildedRose/tests/approval_tests/
-rm -ff demos/03LogFiles/approval_tests/
+rm -rf demos/03LogFiles/approval_tests/
 rm -f  demos/04SVGFiles/approval_tests/*.received.*
 rm -f  demos/04SVGFiles/approval_tests/*.png
 rm -rf demos/05QtWidgets/approval_tests/
