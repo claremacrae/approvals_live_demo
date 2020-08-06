a9e18f308e11a22345be9efabb31a918026572af
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-06 18:24:16 +0100

    Test HelloApprovals (Example 1)

--- demos/01HelloApprovals/HelloApprovalsTests.cpp
+++ demos/01HelloApprovals/HelloApprovalsTests.cpp
@@ -1,21 +1,21 @@
 #include "HelloApprovals.h"
 #include <catch2/catch.hpp>
+#include "ApprovalTests.hpp"
 
 // TODO 1 Run and then fix this test
 
 TEST_CASE( "helloApprovals" )
 {
-    CHECK( HelloApprovals::helloApprovals() == "?" );
+    CHECK( HelloApprovals::helloApprovals() == "Hello Approvals" );
 }
 
-/*
 // TODO 2 Test HelloApprovals::helloPoetry("Vogon")
 
 TEST_CASE( "helloPoetry" )
 {
-    CHECK( HelloApprovals::helloPoetry("Vogon") == "?" );
+    //CHECK( HelloApprovals::helloPoetry("Vogon") == "?" );
+    ApprovalTests::Approvals::verify(HelloApprovals::helloPoetry("Vogon"));
 }
-*/
 
 /*
  https://github.com/approvals/ApprovalTests.cpp/releases
