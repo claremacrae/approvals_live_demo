c46be7d1b1e5761aeb81fe9d533101f544fa14b9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-07 08:15:39 +0100

    Complete example 3 - as demoed

--- demos/03LogFiles/test_class_with_logging.cpp
+++ demos/03LogFiles/test_class_with_logging.cpp
@@ -13,5 +13,15 @@ using namespace ApprovalTests;
 TEST_CASE( "Test ComplexOperation Log" )
 {
     ComplexOperation stuff;
-    stuff.doStuff( std::cout );
+    auto filename = Approvals::getDefaultNamer()->getReceivedFile(".log");
+    {
+        std::ofstream stream( filename );
+        stuff.doStuff( stream );
+    }
+    auto dateRegex = R"(Thu Aug  6 \d\d:\d\d:\d\d 2020)";
+    auto replacement = "[date-and-time]";
+    // Scrubber
+    auto scrubber = Scrubbers::createRegexScrubber(dateRegex, replacement);
+
+    Approvals::verifyExistingFile(filename, Options().withScrubber(scrubber));
 }
