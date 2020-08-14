67076f2b25c4e6a104eaffecf2670b8bfe68a19a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-14 16:33:37 +0100

    Reduce scope of work for demo 3
    To just adding scrubber

--- demos/03LogFiles/test_class_with_logging.cpp
+++ demos/03LogFiles/test_class_with_logging.cpp
@@ -2,15 +2,20 @@
 #include <ApprovalTests.hpp>
 
 #include "ComplexOperation.h"
-#include <iostream>
 
 using namespace ApprovalTests;
 
-// TODO Write a test for the log output from ComplexOperation::doStuff()
-//      to enable refactoring work...
+// TODO Make this test of ComplexOperation::doStuff() pass,
+//      to enable refactoring work to start...
 
 TEST_CASE( "Test ComplexOperation Log" )
 {
     ComplexOperation stuff;
-    stuff.doStuff( std::cout );
+    auto filename = Approvals::getDefaultNamer()->getReceivedFile(".log");
+    {
+        std::ofstream stream( filename );
+        stuff.doStuff( stream );
+    } // close the file
+
+    Approvals::verifyExistingFile(filename);
 }
