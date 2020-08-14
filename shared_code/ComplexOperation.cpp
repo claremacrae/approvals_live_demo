0441a8fdac107ba04228ffb7cabe965fe39c1ec6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-14 18:33:46 +0100

    Switch to entirely numeric date format
    To simplify the demo's regex.
    
    Code taken from Approval Tests

--- shared_code/ComplexOperation.cpp
+++ shared_code/ComplexOperation.cpp
@@ -2,12 +2,49 @@
 #include "Logger.h"
 
 #include <chrono>
+#include <iomanip>
+#include <sstream>
+
+namespace
+{
+    tm toUtc(time_t& tt)
+    {
+#ifdef _MSC_VER // Visual Studio compiler
+        std::tm tm_value = {};
+        gmtime_s(&tm_value, &tt);
+#else
+        tm tm_value = *gmtime(&tt);
+#endif
+        return tm_value;
+    }
+
+    template <typename T> static std::string toString(const T& contents)
+    {
+        std::stringstream s;
+        s << contents;
+        return s.str();
+    }
+
+    std::string toString(const std::chrono::system_clock::time_point& dateTime,
+                         const std::string& format)
+    {
+        time_t tt = std::chrono::system_clock::to_time_t(dateTime);
+        tm tm_value = toUtc(tt);
+
+        return toString(std::put_time(&tm_value, format.c_str()));
+    }
+}
+
+std::string convertSystemClockToString(const std::chrono::time_point<std::chrono::system_clock>& end)
+{
+    const std::string format = "%F %T";
+    return toString(end, format);
+}
 
 void logTimeAndStep( Logger& logger, const std::string& step )
 {
     auto end = std::chrono::system_clock::now();
-    time_t end_time = std::chrono::system_clock::to_time_t( end );
-    logger << step << " at " << ctime( &end_time );
+    logger << step << " at " << convertSystemClockToString(end) << '\n';
 }
 
 void ComplexOperation::doStuff( std::ostream& stream )
