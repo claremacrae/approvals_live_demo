58897a324a48c96a7e6499f3f62f0cb143c44a2a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-14 18:41:47 +0100

    Move some functions to .cpp

---
+++ shared_code/DateUtilities.cpp
@@ -0,0 +1,25 @@
+#include "DateUtilities.h"
+
+namespace DateUtilities
+{
+    tm toUtc( time_t& tt )
+    {
+#ifdef _MSC_VER // Visual Studio compiler
+        std::tm tm_value = {};
+        gmtime_s( &tm_value, &tt );
+#else
+        tm tm_value = *gmtime( &tt );
+#endif
+        return tm_value;
+    }
+
+    std::string
+    toString( const std::chrono::system_clock::time_point& dateTime,
+              const std::string& format )
+    {
+        time_t tt = std::chrono::system_clock::to_time_t( dateTime );
+        tm tm_value = toUtc( tt );
+
+        return toString( std::put_time( &tm_value, format.c_str() ) );
+    }
+}
--- shared_code/DateUtilities.h
+++ shared_code/DateUtilities.h
@@ -7,33 +7,19 @@
 
 namespace DateUtilities
 {
-    tm toUtc(time_t& tt)
-    {
-#ifdef _MSC_VER // Visual Studio compiler
-        std::tm tm_value = {};
-        gmtime_s(&tm_value, &tt);
-#else
-        tm tm_value = *gmtime(&tt);
-#endif
-        return tm_value;
-    }
+    tm toUtc( time_t& tt );
 
-    template <typename T> static std::string toString(const T& contents)
+    template <typename T>
+    static std::string toString( const T& contents )
     {
         std::stringstream s;
         s << contents;
         return s.str();
     }
 
-    std::string toString(const std::chrono::system_clock::time_point& dateTime,
-                         const std::string& format)
-    {
-        time_t tt = std::chrono::system_clock::to_time_t(dateTime);
-        tm tm_value = toUtc(tt);
-
-        return toString(std::put_time(&tm_value, format.c_str()));
-    }
-
+    std::string
+    toString( const std::chrono::system_clock::time_point& dateTime,
+              const std::string& format );
 }
 
 #endif //DEMO_DATEUTILITIES_H
