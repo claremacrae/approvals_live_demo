a938b9a7a12d50f9655f0f437efca287da6ef6a8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-15 15:52:28 +0100

    Use spdlog for all logging, so date-and-time is on every line
    For more realistic log output

--- shared_code/ComplexOperation.cpp
+++ shared_code/ComplexOperation.cpp
@@ -1,43 +1,49 @@
 #include "ComplexOperation.h"
-#include "Logger.h"
 
 #include "DateUtilities.h"
 
 #include <fstream>
 
-std::string convertSystemClockToString(const std::chrono::time_point<std::chrono::system_clock>& end)
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/ostream_sink.h"
+
+std::string convertSystemClockToString(
+    const std::chrono::time_point<std::chrono::system_clock>& end )
 {
     return DateUtilities::toString(end);
 }
 
-void logTimeAndStep( Logger& logger, const std::string& step )
+void logTimeAndStep( spdlog::logger& logger, const std::string& step )
 {
     auto end = std::chrono::system_clock::now();
-    logger << step << " at " << convertSystemClockToString(end) << '\n';
+    logger.info( step + " at " + convertSystemClockToString(end) );
 }
 
 void ComplexOperation::doStuff( std::ostream& stream )
 {
-    Logger logger( stream );
+    auto sink =
+        std::make_shared<spdlog::sinks::ostream_sink_mt>( stream );
+    //    auto logger = std::make_shared<spdlog::logger>("stream_sync", sink);
+    auto logger = spdlog::logger( "ComplexOperation", sink );
 
     logTimeAndStep( logger, "started computation" );
 
-    logger << "Doing step 1" << '\n';
-    logger << "Doing step 2 - part 1" << '\n';
-    logger << "Doing step 2 - part 2" << '\n';
-    logger << "Doing step 3 - testing if part 2 worked" << '\n';
+    logger.info( "Doing step 1" );
+    logger.info( "Doing step 2 - part 1" );
+    logger.info( "Doing step 2 - part 2" );
+    logger.info( "Doing step 3 - testing if part 2 worked" );
     for ( int i = 1; i <= 7; ++i )
     {
-        logger << "Step 4 - section " << i << '\n';
+        logger.info( "Step 4 - section " + std::to_string( i ) );
     }
 
     logTimeAndStep( logger, "finished computation" );
 
-    logger << "Done!\n";
+    logger.info( "Done!" );
 }
 
 void ComplexOperation::doStuff( const std::string& filename )
 {
     std::ofstream outputStream( filename );
-    doStuff(outputStream);
+    doStuff( outputStream );
 }
