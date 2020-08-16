1ad5c4f93fda76691ee213eb592853ec5267f008
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-08-16 13:20:33 +0100

    Make setup_for_demo.sh content consistent

--- setup_for_demo.sh
+++ setup_for_demo.sh
@@ -2,10 +2,10 @@
 
 delete_coverage_gcda_files.sh
 
-rm -f  demos/01HelloApprovals/HelloApprovalsTests.helloPoetry.*.txt
+rm -rf demos/01HelloApprovals/HelloApprovalsTests.helloPoetry.*.txt
 rm -rf demos/02GildedRose/tests/approval_tests/
 rm -rf demos/03LogFiles/*.received.*
 rm -rf demos/03LogFiles/*.approved.*
-rm -f  demos/04SVGFiles/approval_tests/*.received.*
-rm -f  demos/04SVGFiles/approval_tests/*.png
+rm -rf demos/04SVGFiles/approval_tests/*.received.*
+rm -rf demos/04SVGFiles/approval_tests/*.png
 rm -rf demos/05QtWidgets/approval_tests/
