a8ae6199250ff55e0793e6baa4751dcffea71ba1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-09-16 11:48:19 +0100

    Remove image-related code from table tests

--- demos/05QTableWidget/TableWidgetTextTest.cpp
+++ demos/05QTableWidget/TableWidgetTextTest.cpp
@@ -1,13 +1,10 @@
 #include <catch2/catch.hpp>
 #include <ApprovalTestsQt.hpp>
 
-#include <QImage>
 #include <QTableWidget>
 
-#include "helpers/QImageExamples.h"
 #include "helpers/QTableWidgetExamples.h"
 
-using namespace ApprovalTests;
 using namespace ApprovalTestsQt;
 
 // Scenario: Some code that mixes doing some calculations
@@ -21,33 +18,3 @@ TEST_CASE( "It approves a QTableWidget" )
 
     ApprovalTestsQt::verifyQTableView( tableWidget );
 }
-
-/*
-// TODO 3: Run this, and understand the output
-TEST_CASE( "It approves QImages" )
-{
-//    auto defaultReporterDisposer = Approvals::useAsDefaultReporter(
-//        std::make_shared<Mac::BeyondCompareReporter>() );
-
-    std::vector<const char*> colors = {
-        "red", "green", "blue", "purple" };
-    for ( const auto& color : colors )
-    {
-        QImage image = QImageExamples::createImage( color );
-        ApprovalTestsQt::verifyQImage( image );
-    }
-}
- */
-
-/*
-    // TODO 4: Write each image to a separate file
-    // Catch2: SECTION()
-    //  https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/MultipleOutputFilesPerTest.html
-*/
-
-/*
-    // TODO 5: Make it easier to approve the output
-    // a) AutoApproveReporter
-    // b) SeparateApprovedAndReceivedDirectoriesNamer
-    //  https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/Namers.html#separateapprovedandreceiveddirectoriesnamer
-*/
