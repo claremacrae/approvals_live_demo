290d3217e8ef3ef987ede42f37ac20a340489195
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-09-16 12:28:45 +0100

    Simplify QImage test code

--- demos/06QImage/ImageTest.cpp
+++ demos/06QImage/ImageTest.cpp
@@ -9,7 +9,7 @@
 using namespace ApprovalTests;
 using namespace ApprovalTestsQt;
 
-// TODO 3: Run this, and understand the output
+// TODO 1: Run this, and understand the output
 TEST_CASE( "It approves QImages" )
 {
 //    auto defaultReporterDisposer = Approvals::useAsDefaultReporter(
@@ -18,16 +18,3 @@ TEST_CASE( "It approves QImages" )
     QImage image = QImageExamples::createImage( "red" );
     ApprovalTestsQt::verifyQImage( image );
 }
-
-/*
-    // TODO 4: Write each image to a separate file
-    // Catch2: SECTION()
-    //  https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/MultipleOutputFilesPerTest.html
-*/
-
-/*
-    // TODO 5: Make it easier to approve the output
-    // a) AutoApproveReporter
-    // b) SeparateApprovedAndReceivedDirectoriesNamer
-    //  https://approvaltestscpp.readthedocs.io/en/latest/generated_docs/Namers.html#separateapprovedandreceiveddirectoriesnamer
-*/
